1268f43a038319546c2cadbfe7765e6b416d2fdb
Wang Xiaoqiang <xq21@vip.qq.com>
2016-12-27 00:25:39 +0800

    group home page supports action: 位置，说两句，发红包

--- be/transreport/src/main/webapp/fe/js/groupHome.js
+++ be/transreport/src/main/webapp/fe/js/groupHome.js
@@ -1,6 +1,43 @@
 "use strict";
 
-// angular.module('group',[])
-// .controller('groupHomeController',function($scope, $routeParams, $http, $injector){
-//   console.log("access group/" + $routeParams.id);
-// });
+angular.module('group',['ionic'])
+.controller('groupHomeController',function($scope, $routeParams, $http, $injector, $ionicActionSheet){
+  console.log("access group/" + $routeParams.id);
+
+  const GROUP_ACTIONS = {
+    SEND_LOCATION: 0,
+    SAY_SOMETHING: 1,
+    SEND_RED_PACKET: 2
+  };
+
+  $scope.showAction = function() {
+    var hideSheet = $ionicActionSheet.show({
+      buttons: [
+        {text: '<i class="icon ion-arrow-shrink"></i> 位置'},
+        {text: '<i class="icon ion-navicon"></i> 说两句'},
+        {text: '<i class="icon ion-heart"></i> 发红包'}
+      ],
+      cancelText: '取消',
+      cancel: function() {
+        hideSheet();
+      },
+      buttonClicked: function(index) {
+        console.log("action index:", index);
+
+        switch (index) {
+          case GROUP_ACTIONS.SEND_LOCATION:
+            console.log("Action: SEND LOCATION");
+            break;
+          case GROUP_ACTIONS.SAY_SOMETHING:
+            console.log("Action: SAY SOMETHING");
+            break;
+          case GROUP_ACTIONS.SEND_RED_PACKET:
+            console.log("Action: SEND RED PACKET");
+            break;
+        }
+
+        return true;
+      }
+    });
+  };
+});
